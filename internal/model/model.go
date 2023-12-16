5d9da512d03414fc88ec5f39bc3dd794b5604efa
Itay Garin <thifixp@gmail.com>
2023-12-16 14:43:22 +0200

    refactor

--- internal/model/model.go
+++ internal/model/model.go
@@ -20,22 +20,4 @@ type Transaction struct {
 	Hash             string    `json:"hash"`
 }
 
-type GoogleCloudStorageEvent struct {
-	Kind                    string    `json:"kind"`
-	ID                      string    `json:"id"`
-	SelfLink                string    `json:"selfLink"`
-	Name                    string    `json:"name"`
-	Bucket                  string    `json:"bucket"`
-	Generation              string    `json:"generation"`
-	Metageneration          string    `json:"metageneration"`
-	ContentType             string    `json:"contentType"`
-	TimeCreated             time.Time `json:"timeCreated"`
-	Updated                 time.Time `json:"updated"`
-	StorageClass            string    `json:"storageClass"`
-	TimeStorageClassUpdated time.Time `json:"timeStorageClassUpdated"`
-	Size                    string    `json:"size"`
-	Md5Hash                 string    `json:"md5Hash"`
-	MediaLink               string    `json:"mediaLink"`
-	Crc32c                  string    `json:"crc32c"`
-	Etag                    string    `json:"etag"`
-}
+type TransactionsFileUploadedEvent struct{}
